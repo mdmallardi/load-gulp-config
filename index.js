bc47a67fb04fbed5ae2704e55a186c56df39c522
Adrian Miranda <adriancmiranda@gmail.com>
2016-05-27 19:49:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ var path = require('path');
 // @see https://www.npmjs.com/package/glob
 var glob = require('glob');
 
+// YAML 1.2 parser and serializer.
+// @see https://www.npmjs.com/package/js-yaml
+var YAML = require('js-yaml');
+
 // Synchronous version of `fs.readFile`. Returns the contents of the file and parse to JSON.
 // @see https://nodejs.org/api/fs.html#fs_fs_readfilesync_file_options
 function readJSON(filepath){
@@ -20,6 +24,21 @@ function readJSON(filepath){
     buffer = JSON.parse(fs.readFileSync(filepath, { encoding:'utf8' }));
   } catch(error){}
   return buffer;
+}
+
+// Synchronous version of `fs.readFile`. Returns the contents of the file and parse to YAML.
+// @see https://nodejs.org/api/fs.html#fs_fs_readfilesync_file_options
+function readYAML(filepath){
+  var buffer = {};
+  try{
+    buffer = YAML.safeLoad(fs.readFileSync(filepath, 'utf8'));
+  }catch(error){}
+  return buffer;
+}
+
+function configure(aliases){
+  loadGulpConfig.aliases = aliases;
+  console.log('aliases:', aliases);
 }
 
 // Define a task using [Orchestrator](https://github.com/robrich/orchestrator).
@@ -41,12 +60,15 @@ function iteraction(gulp, options, taskFile){
     });
     gulp.task(filename, cmds);
   }
+  configure(options.aliases);
 }
 
 // Load multiple gulp tasks using globbing patterns.
 function loadGulpConfig(gulp, options){
   options = Object.assign({ data:{} }, options);
   options.configPath = typeof options.configPath === 'string'? options.configPath : 'tasks';
+  options.aliases = options.aliases === Object(options.aliases)? options.aliases : null;
+  options.aliases = options.aliases || readYAML(path.join(options.configPath, 'aliases.yml'));
   glob.sync(options.configPath, { realpath:true }).forEach(iteraction.bind(this, gulp, options));
 }
 
